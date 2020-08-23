04546eec0e2f2bc34b3fb6cf8f25401c62e08bba
ctsdotnetuser111 <ctsdotnetuser111@CTSDOTNET26>
2020-08-23 13:01:53 +0530

    Added swagger and removed hardcoded connection strings

--- LoanManagement/Startup.cs
+++ LoanManagement/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace LoanManagement
 {
@@ -28,7 +29,13 @@ namespace LoanManagement
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddDbContext<LoanDBContext>(option => option.UseSqlServer(@"Data Source=CTSDOTNET26;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False; Initial Catalog=FinalLoanManagementDb;"));
+            //services.AddDbContext<LoanDBContext>(option => option.UseSqlServer(@"Data Source=CTSDOTNET26;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False; Initial Catalog=FinalLoanManagementDb;"));
+            services.AddDbContext<LoanDBContext>(option => option.UseSqlServer(Configuration.GetConnectionString("FinalLoanManagementDb")));
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +45,15 @@ namespace LoanManagement
             {
                 app.UseDeveloperExceptionPage();
             }
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseHttpsRedirection();
 
--- UserManagement/Startup.cs
+++ UserManagement/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using UserManagement.Data;
 
 namespace UserManagement
@@ -28,7 +29,12 @@ namespace UserManagement
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddDbContext<UserDBContext>(option => option.UseSqlServer(@"Data Source=CTSDOTNET26;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False; Initial Catalog=FinalUserDB;"));
+            //services.AddDbContext<UserDBContext>(option => option.UseSqlServer(@"Data Source=CTSDOTNET26;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False; Initial Catalog=FinalUserDB;"));
+            services.AddDbContext<UserDBContext>(option => option.UseSqlServer(Configuration.GetConnectionString("FinalUserDB")));
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,7 +44,15 @@ namespace UserManagement
             {
                 app.UseDeveloperExceptionPage();
             }
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
 
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
             app.UseHttpsRedirection();
 
             app.UseRouting();
